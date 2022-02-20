bc72c6adcfe6b6709be83d097672dbb8ad3aac50
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2022-02-20 19:52:29 -0300

    radix popover

---
+++ packages/ui/Popover.tsx
@@ -0,0 +1,10 @@
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+import { styled } from './stitches.config';
+
+export const Popover = PopoverPrimitive.Root;
+export const PopoverTrigger = PopoverPrimitive.Trigger;
+export const PopoverContent = styled(PopoverPrimitive.Content, {
+  border: "solid 1px #ccc",
+  background: "white",
+  padding: "1em",
+});
